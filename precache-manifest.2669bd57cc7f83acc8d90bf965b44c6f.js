80736e57010161f0054560e839dc3361eb885634
Denis Radchenko <denys.radchenko.v@gmail.com>
2019-05-09 17:43:25 +0200

    Updates

---
+++ precache-manifest.2669bd57cc7f83acc8d90bf965b44c6f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597d208b0a004d9fac7dbed7bc6ca812",
+    "url": "/minesweeper-game/index.html"
+  },
+  {
+    "revision": "c47cf8710a19c352d243",
+    "url": "/minesweeper-game/static/css/2.73af3247.chunk.css"
+  },
+  {
+    "revision": "6fc7fd337bd938058106",
+    "url": "/minesweeper-game/static/css/main.fa47e3e8.chunk.css"
+  },
+  {
+    "revision": "c47cf8710a19c352d243",
+    "url": "/minesweeper-game/static/js/2.466828ee.chunk.js"
+  },
+  {
+    "revision": "6fc7fd337bd938058106",
+    "url": "/minesweeper-game/static/js/main.38534032.chunk.js"
+  },
+  {
+    "revision": "f44b15362e1324e3485d",
+    "url": "/minesweeper-game/static/js/runtime~main.6e6024fd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/minesweeper-game/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/minesweeper-game/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/minesweeper-game/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/minesweeper-game/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/minesweeper-game/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
